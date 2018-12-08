f7f215a19fb9b418efc8cb1db2bcb48241ce235a
Johnpaul Raphel <paul.jopal@gmail.com>
2018-12-09 05:19:19 +0530

    third fix try for heroku build deployment

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
+var path = require('path');
 const app = express();
 app.use(express.static(__dirname+'/dist'));
+console.log('dirname: '+__dirname+'/dist');
 app.listen(process.env.PORT || 8080);
 
 //Path location starategy
 
 app.get('/*',function(req,res){
-  res.sendFile(path.join(__dirname+'/dist/index.html'));
+    console.log('req '+req);
+    console.log('res '+res);
+    console.log('path join: '+path.join(__dirname+'/dist/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/angular-CRUD-app/index.html'));
 });
 
 console.log('console listening!!');
